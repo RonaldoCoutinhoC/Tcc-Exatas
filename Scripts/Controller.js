8cf2cdc43f26e116fc54ef45b7376678c9dce9ad
Ronaldo <ronaldo.coutinho.dev@gmail.com>
2023-01-19 08:41:26 -0300

    Minor Changes

--- Scripts/Controller.js
+++ Scripts/Controller.js
@@ -4,7 +4,7 @@ import { SaveController } from "./Model/SaveController.js";
 
 function startGame(){
     SaveController.loadGame();
-    Level.startLevel(Level.CURRENT_LEVEL);
+    Level.startLevel(Level.CURRENT_LEVEL);//aa
     Button.setButtons(Button.MAIN_MENU_BUTTONS);
     Button.setHelperJoshButtons(Button.HELPER_JOSH_BUTTONS);
 }
